5942376a9346e91589e1e474fec4f3c69e6e4f06
Adrian <66633552+Adrian-Capbatut@users.noreply.github.com>
2023-05-09 21:58:50 +0300

    Changing the time and assigning innerHtml

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const intems = document.querySelectorAll(".countdown-item > h4");
 
+const countdownElement = document.querySelector(".countdown");
+
 // Incepem timpul ramas pana la lansare
-let countdownDate = new Date(2023, 06, 09, 14, 5, 0).getTime();
+let countdownDate = new Date(2023, 06, 09, 21, 52, 0).getTime();
 
 function getCountdouwnTime() {
     //Determinarea timpului real
@@ -22,24 +24,27 @@ function getCountdouwnTime() {
 
     //
     let days = Math.floor(distance / oneDay);
-    let hours = Math.floor((distance % oneDay) / oneHour)
-    let minutes = Math.floor((distance % oneHour) / oneMinute)
-    let secundes = Math.floor((distance % oneMinute) / 1000)
+    let hours = Math.floor((distance % oneDay) / oneHour);
+    let minutes = Math.floor((distance % oneHour) / oneMinute);
+    let secundes = Math.floor((distance % oneMinute) / 1000);
 
     //Creem un masiv cu variabile
     const values = [days, hours, minutes, secundes];
-    console.log(values)
+    console.log(values);
 
     //Adaugam valoare variabililor de pe pagine
     intems.forEach(function (item, index) {
-        item.textContent = (values[index])
-    })
-}
+        item.textContent = values[index];
+    });
 
+    if (distance < 0) {
+        clearInterval(countdown);
+        countdownElement.innerHTML = "<h4 class='expired'> Timpul a trecut </h4>";
+    }
+}
 
 // Actualizare in fiecare secunda
 let countdown = setInterval(getCountdouwnTime, 1000);
-
 
 // Initializarea timpului curent
 getCountdouwnTime();
